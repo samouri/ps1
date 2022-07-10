735b4ca511caef766db6047d83b72ff5da0386a9
Jake <4656974+samouri@users.noreply.github.com>
2022-07-10 12:33:14 -0700

    use %F and %f for colors, Mac builtin Terminal doesnt support RGB based colors

--- src/main.rs
+++ src/main.rs
@@ -27,11 +27,11 @@ fn main() -> std::io::Result<()> {
     let branch = get_branch();
 
     let mut ps1: Vec<String> = Vec::new();
-    ps1.push(cyan(cwd.to_string_lossy().to_string()));
+    ps1.push(blue(cwd.to_string_lossy().to_string()));
     if !branch.is_empty() {
         ps1.push(grey(branch))
     }
-    ps1.push(pink(LAMBDA.to_owned()));
+    ps1.push(magenta(LAMBDA.to_owned()));
 
     print!("{} ", ps1.join(" "));
     Ok(())
@@ -58,17 +58,37 @@ mod colors {
     #[derive(Debug)]
     pub struct Color(u8, u8, u8);
 
+    pub fn blue(msg: String) -> String {
+        colorize("blue", msg)
+    }
     pub fn cyan(msg: String) -> String {
-        colorize(Color(118, 196, 250), msg)
+        colorize("cyan", msg)
     }
     pub fn grey(msg: String) -> String {
-        colorize(Color(108, 108, 108), msg)
+        colorize("242", msg)
     }
-    pub fn pink(msg: String) -> String {
-        colorize(Color(237, 115, 190), msg)
+    pub fn magenta(msg: String) -> String {
+        colorize("magenta", msg)
     }
 
-    pub fn colorize(col: Color, msg: String) -> String {
+    pub fn colorize(col: &str, msg: String) -> String {
+        let reset_esc = "%f";
+        let color_esc = format!("%F{{{}}}", col);
+        format!("{color_esc}{msg}{reset_esc}")
+    }
+
+    pub fn cyan_rgb(msg: String) -> String {
+        colorize_rgb(Color(118, 196, 250), msg)
+    }
+    pub fn grey_rgb(msg: String) -> String {
+        colorize_rgb(Color(108, 108, 108), msg)
+    }
+    pub fn pink_rgb(msg: String) -> String {
+        colorize_rgb(Color(237, 115, 190), msg)
+    }
+
+    // Print colors using RGB -- only
+    pub fn colorize_rgb(col: Color, msg: String) -> String {
         let Color(red, green, blue) = col;
         // TODO: is reset diff on windows?
         let reset_esc = "%{\x1b[0m%}";
